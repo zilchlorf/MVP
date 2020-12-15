aee72af3e38e195b7944c4c0e5da3414320b414d
Max Edmonds <maxedmonds05@gmail.com>
2020-12-14 21:53:21 -0800

    establish folder structure for editability

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'index.scss';
+
+import { App } from 'ui/app';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
---
+++ src/ui/app.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from 'ui/logo.svg';
+
+export const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/ui/app.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
